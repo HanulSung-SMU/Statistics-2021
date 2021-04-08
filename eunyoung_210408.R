f0c3c49853686c76a1444b46697845186d7a0660
EunYoung Kang <201811140@sangmyung.kr>
2021-04-08 14:02:21 +0900

    eunyoung 0408Practice add median

--- eunyoung_210408.R
+++ eunyoung_210408.R
@@ -1,3 +1,5 @@
+#평균값
+
 mean(c(1:50))
 
 DF_student_exam <- read.csv(file = "student_exam.csv")
@@ -8,11 +10,28 @@ DF_coffee_location <- read.csv(file = "coffee_monthly_location_sales.csv")
 mean(No_Busan)
 mean(No_Seoul)
 mean(No_Gangneung)
-
 ----------------------------------------------------
+#중앙값
+median(c(1,10,5,8,9,20))
+median(c(1:4))
+  
+#DF_student_exam의 중앙값
+median(Class_A)
+median(Class_B)
+
+#DF_coffee_location의 중앙값
+median(No_Busan)
+median(No_Seoul)
+median(No_Gangneung)
+  
+----------------------------------------------------
+#분산
+
+#DF_student_exam의 분산
 var(Class_A)
 var(Class_B)
 
+#DF_coffee_location의 분산
 var(No_Busan)
 var(No_Seoul)
 var(No_Gangneung)
