b3a02c44d20078638aa356520508798de89dbf0f
lgh9776 <lgh9776@naver.com>
2021-04-12 00:29:53 +0900

    file upload

---
+++ 202010958_4_8.R
@@ -0,0 +1,9 @@
+DF_coffee_location = read.csv(file = "coffee_monthly_location_sales.csv")
+DF_student_exam = read.csv(file = "student_exam.csv")
+
+var(DF_student_exam$Class_A)
+var(DF_student_exam$Class_B)
+
+var(DF_coffee_location$No_Gangneung)
+var(DF_coffee_location$No_Seoul)
+var(DF_coffee_location$No_Busan)
