970e85d03ee19d907a010fce0306f9b7c7b4e824
자연어처리 <leejaewook133@gmail.com>
2021-04-14 14:48:51 +0900

    20210414 수업내용 추가

--- 20210408_statistics.R
+++ 20210408_statistics.R
@@ -7,3 +7,74 @@ df2 <- read.csv("coffee_monthly_location_sales.csv")
 busan <- var(df2$No_Busan)
 seoul <- var(df2$No_Seoul)
 gangneung <- var(df2$No_Gangneung)
+
+df3 <- read.csv("blog_visitor.csv")
+range(df3$Visitors)
+
+range(df$Class_A)
+range(df$Class_B)
+
+summary(df$Class_A)
+summary(df$Class_B)
+
+
+# ===============연습문제================#
+# 3번
+DF_mobile_usage <- read.csv("mobile_usage.csv")
+mean(DF_mobile_usage$Usage_time)
+
+# 4번
+median(DF_mobile_usage$Usage_time)
+# 평균 : 3.279688 / 중앙값 : 3.5 
+# -> 데이터의 분포가 중앙값보다 아래로 치우쳐짐
+
+# 5번
+var(DF_mobile_usage$Usage_time)
+sd(DF_mobile_usage$Usage_time)
+
+# 6번
+min(DF_mobile_usage$Usage_time)
+max(DF_mobile_usage$Usage_time)
+
+# 7번
+summary(DF_mobile_usage$Usage_time)
+
+# 확률분포표 (prop.table 사용)
+a <- c(1, 2, 3, 4, 5, 6)
+b <- c(1, 2, 3, 4, 5, 6)
+
+df2 <- data.frame(cnt=c(0, 0, 0))
+
+for(x in a){
+  for (y in b){
+    if (x == 1){
+      if (y == 1){
+        two <- two + 1
+        df2$cnt[3] <- df2$cnt[3]+1
+        }
+      else{
+        df2$cnt[2] <- df2$cnt[2]+1
+      }
+    }
+    else{
+      if (y == 1){
+        df2$cnt[2] <- df2$cnt[2]+1
+      }
+      else {
+        df2$cnt[1] <- df2$cnt[1]+1
+      }
+    }
+  }
+}
+
+View(prop.table(df2))
+
+View(df2)
+
+# dbinom(x, size, p)
+dbinom(0, 10, 10/1000)+dbinom(1, 10, 10/1000) #불량품이 0개+1개
+dbinom(0, 2, 1/6) # 주사위 2개 던져서 1 나오는 개수가 0개
+
+# pbinom(x, size, p)
+pbinom(1, 10, 0.01) # x는 x회 이하
+pbinom(100-91, 100, 1-0.8)
